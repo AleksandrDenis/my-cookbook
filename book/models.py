0c7e8c6ba1d18c22735aba9729abcbf9212b5207
DenisKudriavcev <deniskudriavcev@gmail.com>
2022-09-13 21:21:52 +0000

    Rename filed commenton

--- book/models.py
+++ book/models.py
@@ -37,11 +37,11 @@ class Comment(models.Model):
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["date_posted"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
