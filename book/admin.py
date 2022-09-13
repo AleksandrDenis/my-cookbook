41fefa794a7f1203dbcff7a2e8b45eec4a5288a3
DenisKudriavcev <deniskudriavcev@gmail.com>
2022-09-13 21:12:18 +0000

    Add mthod to approve commets

--- book/admin.py
+++ book/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
+    prepopulated_fields = {'slug': ('title',)}
+
     summernote_fields = ('ingredients', 'method')
 
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    actions = ['approve_comments']
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+    
 # Register your models here.
