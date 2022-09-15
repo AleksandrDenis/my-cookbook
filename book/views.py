86d06bf3d1e4aa5ef40645c813d54e75be2a1476
DenisKudriavcev <deniskudriavcev@gmail.com>
2022-09-15 19:35:36 +0000

    Create post view list

--- book/views.py
+++ book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-date_posted')
+    template_name = 'index.html'
 # Create your views here.
